cc43676fe070b39d2ba8ed6de38291148dce9c6b
Ashish Chandwani <ashish.s.chandwani@gmail.com>
2016-04-21 21:31:29 -0700

    Add view and utility functions to RandomGifList
    Lots of documentation for Signal.message composition and
    various notes about Json decoding

--- examples/random-gif-viewer-list/RandomGifList.elm
+++ examples/random-gif-viewer-list/RandomGifList.elm
@@ -77,5 +77,77 @@ update message model =
             |> List.unzip
       in
         ( { model | gifList = newGifList }
+          -- most of these are Effects.none
         , batch fxList
         )
+
+
+
+-- VIEW
+-- => is frustrating, but the tutorial included it
+
+
+(=>) : a -> b -> ( a, b )
+(=>) =
+  (,)
+
+
+view : Signal.Address Action -> Model -> Html
+view address model =
+  div
+    []
+    [ input
+        [ placeholder "What kind of gifs do you want"
+        , value model.topic
+        , onEnter address Create
+          {- on : String -> Decoder String(a) -> (String(a) -> Message) -> Attribute
+          Topic : String -> Action
+          Signal.message : Address Action(a) -> Action(a) -> Message
+           so (Signal.message address << Topic) becomes  String -> Message
+          on's 3rd arg needs to be String(a) -> Message because
+          targetValue is Decoder String(a)
+          in plain terms,
+          on needs a way to send the decoded string to the address
+          The function we define here converts the string to an action
+          and creates a message for the address with that Action tagged string
+          -}
+        , on "input" targetValue (Signal.message address << Topic)
+        , inputStyle
+        ]
+        []
+    , div
+        [ style [ "display" => "flex", "flex-wrap" => "wrap" ] ]
+        (List.map (elementView address) model.gifList)
+    ]
+
+
+elementView : Signal.Address Action -> ( Int, RandomGif.Model ) -> Html
+elementView address ( id, model ) =
+  RandomGif.view (Signal.forwardTo address (SubMsg id)) model
+
+
+inputStyle : Attribute
+inputStyle =
+  style
+    [ ( "width", "100%" )
+    , ( "height", "40px" )
+    , ( "padding", "10px 0" )
+    , ( "font-size", "2em" )
+    , ( "text-align", "center" )
+    ]
+
+
+onEnter : Signal.Address a -> a -> Attribute
+onEnter address value =
+  let
+    is13 code =
+      if code == 13 then
+        Ok ()
+      else
+        Err "not the right key code"
+  in
+    on
+      "keydown"
+      -- Decoder ()
+      (Json.customDecoder keyCode is13)
+      (\_ -> Signal.message address value)
